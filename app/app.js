1345e11fb00a129b40422033b06fe8e25908a1e0
Jayden Rasmussen <jayden.rasmussen@gmail.com>
2018-03-01 15:41:48 -0700

    updatd the help section with better syntax

--- app/app.js
+++ app/app.js
@@ -4,14 +4,14 @@ const parse = require('./parse');
 module.exports = (async () => {
     const cli = meow(
         `Usage: qg [command] <flag> [project name]
-Commands: electron <e> - generates an electron app
-          go <g> - generates a go app
-          init <i> - generates a project directory
-          node <n> - generates a node app
-          micro <m> - generates micro app
-          polka <p> - generates polka app
-          express <e> - generates an express app
-          micro <m> - generates a zeit/micro app
+Commands: electron [e] - generates an electron app
+          go [g] - generates a go app
+          init [i] - generates a project directory
+          node [n] - generates a node app
+          micro [m] - generates micro app
+          polka [p] - generates polka app
+          express [e] - generates an express app
+          micro [m] - generates a zeit/micro app
 Flags: repo <r>
 
 Examples: qg n -r sweet-tea`,
